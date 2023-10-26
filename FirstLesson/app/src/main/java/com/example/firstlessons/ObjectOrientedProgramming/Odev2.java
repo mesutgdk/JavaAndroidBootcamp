28e19380f6930986842c5b6d7bef108f41fe5f40
mesutgdk <mesuttgedik@gmail.com>
2023-10-26 13:55:45 +0300

    second lesson and a part of odev2

--- FirstLesson/app/src/main/java/com/example/firstlessons/ObjectOrientedProgramming/Odev2.java
+++ FirstLesson/app/src/main/java/com/example/firstlessons/ObjectOrientedProgramming/Odev2.java
@@ -1,7 +1,28 @@
 package com.example.firstlessons.ObjectOrientedProgramming;
 
 public class Odev2 {
-    public float question1(float km){
-    
+    // Km to Miles
+    public double question1(double km){
+        double miles = km * 0.621;
+        return  miles;
+    }
+    // lateral to area, kenar uzunluğundan alana
+    public int question2(int lateral1, int lateral2){
+        int area = lateral1*lateral2;
+        return  area;
+    }
+    public int question3(int num) {
+        if (num < 0) {
+            System.out.println("Negatif sayının faktoriyeli olmaz");
+            return -1; //
+        } else if (num == 0 || num == 1) {
+            return 1;
+        } else {
+            int result = 1;
+            for (int i = 2; i <= num; i++) {
+                result *= i;
+            }
+            return result;
+        }
     }
 }
--- FirstLesson/app/src/main/java/com/example/firstlessons/ObjectOrientedProgramming/Odev2Main.java
+++ FirstLesson/app/src/main/java/com/example/firstlessons/ObjectOrientedProgramming/Odev2Main.java
@@ -3,6 +3,18 @@ package com.example.firstlessons.ObjectOrientedProgramming;
 public class Odev2Main {
     public static void main(String[] args) {
         Odev2 o2 = new Odev2();
-        o2.question1()
+
+        double answerOfMiles = o2.question1(10);
+        System.out.println("Çevirilen Mil: " + answerOfMiles + " miles");
+
+        int areaOfRectangle = o2.question2(4,6);
+        System.out.println("Dikdöergenin Alanı: " + areaOfRectangle + " cm^2");
+
+        int factorial = o2.question3(6);
+        System.out.println("Girilen Sayının Faktöriyeli: " + factorial);
+
+
     }
+
+
 }
