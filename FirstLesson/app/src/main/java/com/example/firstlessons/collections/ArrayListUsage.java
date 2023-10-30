0909252d8724fd5d904e24fb6e43d12ee43722d6
mesutgdk <mesuttgedik@gmail.com>
2023-10-30 19:21:26 +0300

    learning arrays

---
+++ FirstLesson/app/src/main/java/com/example/firstlessons/collections/ArrayListUsage.java
@@ -0,0 +1,16 @@
+package com.example.firstlessons.collections;
+
+import java.util.ArrayList;
+
+public class ArrayListUsage {
+    public static void main(String[] args) {
+        ArrayList<String> meyveler = new ArrayList<>();
+
+        // Veri Ekleme
+        meyveler.add("Muz");
+        meyveler.add("Armut");
+        meyveler.add("Kiraz");
+
+        System.out.println(meyveler);
+    }
+}
