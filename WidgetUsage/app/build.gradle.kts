987c90ff7a2561724b91a15fa61a80f1cff4f5c0
mesutgdk <mesuttgedik@gmail.com>
2023-11-08 19:31:32 +0300

    txview and button with changing photo

--- WidgetUsage/app/build.gradle.kts
+++ WidgetUsage/app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.widgetusage"
-    compileSdk = 33
+    compileSdk = 34
 
 
     buildFeatures{
@@ -13,7 +13,7 @@ android {
     defaultConfig {
         applicationId = "com.example.widgetusage"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
--- WidgetUsage/app/src/main/java/com/example/widgetusage/MainActivity.java
+++ WidgetUsage/app/src/main/java/com/example/widgetusage/MainActivity.java
@@ -16,6 +16,18 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        // edittexten veri alma
+        binding.buttonOku.setOnClickListener(view -> {
+            String alinanVeri = binding.editTextGirdi.getText().toString();
+            binding.textViewSonuc.setText(alinanVeri); // text'e yazdırma
+        });
+        binding.button2Happy.setOnClickListener(view -> {
+            binding.imageView.setImageResource(R.drawable.happy);  // tuş ile resim değiştirme, dosyadan gösterme
+        });
+        binding.button2Sad.setOnClickListener(view -> {
+            binding.imageView.setImageResource(getResources().getIdentifier("sad","drawable",getPackageName()));
+        });
+
 
     }
 }
