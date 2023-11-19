ba96247304a9170339b4eda04f4fe594bb9cc4d7
mesutgdk <mesuttgedik@gmail.com>
2023-11-19 09:23:10 +0300

    data for dolap app is complited

--- DolapApp/app/src/main/java/com/example/dolapapp/ui/fragment/MainScreenFragment.java
+++ DolapApp/app/src/main/java/com/example/dolapapp/ui/fragment/MainScreenFragment.java
@@ -29,7 +29,7 @@ public class MainScreenFragment extends Fragment {
         Products p2 = new Products(2,"Bebek kazak takım","kazak","18-24 ay/92cm","Bebek&Çocuk Alt&Üst Takım","Defacto marka, ürünlerimiz sıfır ürünlerdir..","Yeni/Etiketli","Diğer","K.Rengi",14,150);
         Products p3 = new Products(3,"Külotlu çorap","corap","24-36 ay","Bebek&Çocuk Eldiven&Çorap","LCW marka külotlu çorap, alana hayırlı olsun.","Yeni&Etiketli","LC Waikiki","Beyaz",39,75);
         Products p4 = new Products(4,"ELBİSE","elbise","M/38","Kadın Ofis Elbisesi","Hiçbir kusuru yoktur, birazcık rengi solmuştur, oda fotoda belli oluyor.","Az kullanılmış","Mango","Pembe",99,210);
-        Products p5 = new Products(5,"Ycoo Robot Köpek","kopek"," ","Bebek&Çocuk Kumanda & Akülü araçlar","Kızımın ilgisini hiç çekmedi, eşim adeta kendisi için alıd, ikisi de oynamadı :)","Az kullanılmış","Diğer","Gri",58,650);
+        Products p5 = new Products(5,"Ycoo Robot Köpek","kopek"," ","Bebek&Çocuk Kumanda & Akülü Araçlar","Kızımın ilgisini hiç çekmedi, eşim adeta kendisi için aldı, ikisi de oynamadı :)","Az kullanılmış","Diğer","Gri",58,650);
         Products p6 = new Products(6,"Zara marka pantolon","pantolon","18-24 ay/92cm","Bebek&Çocuk Pantolon","Ürünlerimiz ihracat fazlası olup sıfırdır. İç etiketleri kesik olabilir","Yeni&Etiketli","Zara","Desenli",64,180);
         Products p7 = new Products(7,"Aslan Baskılı Erkek Bebek Takım","renklitakim","12-18 ay/86cm","Bebek&Çocuk Takım","Yıkama derecesi: 30C. Elde yıkama yapmayınız, %100 pamuktan üretilmiştir.","Yeni&Etiketli","Diğer","Bej",28,39);
 
