1e9cfe3544c71289234390e318765f0917875be5
mesutgdk <mesuttgedik@gmail.com>
2023-11-27 20:30:04 +0300

    feat: new icon for map location

--- MapUsage/app/src/main/java/com/example/mapusage/MainActivity.java
+++ MapUsage/app/src/main/java/com/example/mapusage/MainActivity.java
@@ -11,6 +11,8 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -32,7 +34,8 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         binding.buttonKonumaGit.setOnClickListener(v -> {
             //37.555572,34.9388673
             LatLng kamisli = new LatLng(37.555572,34.9388673);
-            mMap.addMarker(new MarkerOptions().position(kamisli).title("Kamışlı"));
+            mMap.addMarker(new MarkerOptions().position(kamisli).title("Kamışlı")
+                    .icon(BitmapDescriptorFactory.fromResource(R.drawable.imlec))); // ikon değiştirme
             mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(kamisli,17f));
             mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
         });
