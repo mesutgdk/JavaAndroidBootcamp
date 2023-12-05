9102a615fb7c0e517885d082d608bfecc2c45dfe
mesutgdk <mesuttgedik@gmail.com>
2023-12-05 14:28:46 +0300

    delete: doy doy

--- 2 DoyDoy/app/src/main/java/com/example/doydoy/di/HiltApplication.java
+++
@@ -1,9 +0,0 @@
-package com.example.doydoy.di;
-
-import android.app.Application;
-
-import dagger.hilt.android.HiltAndroidApp;
-
-@HiltAndroidApp
-public class HiltApplication extends Application {
-}
